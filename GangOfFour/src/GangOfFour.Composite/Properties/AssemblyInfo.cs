77aff2f1de732c94008207b0a7b85a6c2c294a3c
adam144dev <adam144dev@gmail.com>
2017-01-09 18:32:20 +0100

    GangOfFour initialization

---
+++ GangOfFour/src/GangOfFour.Composite/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("adam144dev@gmail.com")]
+[assembly: AssemblyProduct("GangOfFour.Composite")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c678435a-faa6-41b0-bdd0-205d020b92ec")]
