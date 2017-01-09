76048892cca71ec21ad6d55700bdb8409a6ad66d
adam144dev <adam144dev@gmail.com>
2017-01-09 19:02:46 +0100

    Graphic C&C

---
+++ GangOfFour/src/GangOfFour.Composite/GraphicComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace adam144.GangOfFour.Composite
+{
+    /// <summary>
+    /// The 'Component' interface & base class
+    /// </summary>
+
+    public interface IGraphicComponent
+    {
+        IGraphicComposite GetComposite();
+        void Display(int indent);
+    }
+
+    public abstract class GraphicComponent : IGraphicComponent
+    {
+        public virtual IGraphicComposite GetComposite() => null;
+
+        protected string Name { get; }
+
+        protected GraphicComponent(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Display(int indent)
+            => Console.WriteLine(new string('-', indent) + " " + Name);
+    }
+}
---
+++ GangOfFour/src/GangOfFour.Composite/GraphicComposite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace adam144.GangOfFour.Composite
+{
+    /// <summary>
+    /// The 'Composite' interface & base class
+    /// </summary>
+
+    public interface IGraphicComposite
+    {
+        void Add(IGraphicComponent d);
+        void Remove(IGraphicComponent d);
+    }
+
+    public abstract class GraphicComposite : GraphicComponent, IGraphicComposite
+    {
+        private readonly List<IGraphicComponent> elements = new List<IGraphicComponent>();
+
+        protected GraphicComposite(string name)
+            : base(name)
+        {
+        }
+
+
+        /// <summary>
+        /// IDrawingElementComponent
+        /// </summary>
+
+        public virtual void Add(IGraphicComponent d)
+            => elements.Add(d);
+
+        public virtual void Remove(IGraphicComponent d)
+            => elements.Remove(d);
+
+
+        /// <summary>
+        /// IDrawingElementComposite
+        /// </summary>
+
+        public override IGraphicComposite GetComposite() => (IGraphicComposite)this;
+
+        public override void Display(int indent)
+        {
+            Console.WriteLine(new string('-', indent) + "+ " + Name);
+
+            // Display each child element on this node
+            foreach (var d in elements)
+            {
+                d.Display(indent + 2);
+            }
+        }
+    }
+}
